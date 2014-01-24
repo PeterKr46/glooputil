8f17a2fa8ed66ee86f26b29eb4f8c630df1c9862
peter <kr4mer.p@gmail.com>
2014-01-24 21:22:28 +0100

    Readded files

---
+++ src/java/main/com/gmail/pkr4mer/glooputil/Scene.java
@@ -0,0 +1,41 @@
+package com.gmail.pkr4mer.glooputil;
+
+import GLOOP.GLKamera;
+import GLOOP.GLObjekt;
+
+import java.util.HashMap;
+
+/**
+ * Created by peter on 1/24/14.
+ */
+public abstract class Scene
+{
+    private HashMap<String, GLObjekt> objekte;
+    private boolean running;
+
+    public Scene()
+    {
+        objekte = new HashMap<>();
+        running = true;
+    }
+
+    private void mainThread()
+    {
+        while(running)
+        {
+
+        }
+    }
+
+    public GLKamera erstelleKamera()
+    {
+        return erstelleKamera(0, 0, 0);
+    }
+      
+    public GLKamera erstelleKamera(double x, double y, double z)
+    {
+        GLKamera k = new GLKamera();
+        k.setzePosition(x,y,z);
+        return k;
+    }
+}
